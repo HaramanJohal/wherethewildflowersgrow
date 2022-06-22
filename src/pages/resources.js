1afbb99140c473ee6690241f34b3c7e7d95886c4
Haraman Johal <hjohal@oni.bio>
2022-06-23 00:50:59 +0100

    fix links

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -12,37 +12,37 @@ const ResourcesPage = () => {
           <p className={resources}>(Twitter handles are provided but these organisations can all be found on Instagram too) </p>
 
           <p className={resources}><i>Botanical Society of Britain and Ireland (BSBI)</i></p>
-          <a className={link} href="www.bsbi.org"><p className={resources}>www.bsbi.org</p></a>
+          <a className={link} href="https://www.bsbi.org"><p className={resources}>www.bsbi.org</p></a>
           <p className={resources}>@BSBIbotany</p>
           <p className={resources}>The first organisation to become familiar with if you’re interested in wild plants. It’s a very friendly charity with a supportive community of botanists. Their website offers a wealth of information on plant distributions, how to become a beginner plant spotter, and how to find botanists in your local area.</p>
 
           <p className={resources}><i>Plantlife</i></p>
-          <a className={link} href="www.plantlife.org.uk"><p className={resources}>www.plantlife.org.uk</p></a>
+          <a className={link} href="https://www.plantlife.org.uk"><p className={resources}>www.plantlife.org.uk</p></a>
           <p className={resources}>@Love_plants</p>
           <p className={resources}>Plantlife is our biggest charity dedicated to conserving wild plants and suggest a great range of ways to begin botanising on their website. They offer talks, workshops and courses as well as providing toolkits for things like turning your lawn or local park into a wildflower meadow.</p>
 
           <p className={resources}><i>The Species Recovery Trust</i></p>
-          <a className={link} href="www.speciesrecoverytrust.org.uk"><p className={resources}>www.speciesrecoverytrust.org.uk</p></a>
+          <a className={link} href="https://www.speciesrecoverytrust.org.uk"><p className={resources}>www.speciesrecoverytrust.org.uk</p></a>
           <p className={resources}>@speciesrecovery</p>
           <p className={resources}>A brilliant charity working to conserve our most threatened species. They run a series of courses (including one or two taught by me!) for beginners and intermediates. Dominic Price is an excellent tutor and has taught me everything I know about grasses, and much more.</p>
 
           <p className={resources}><i>The Wildlife Trusts</i></p>
-          <a className={link} href="www.wildlifetrusts.org"><p className={resources}>www.wildlifetrusts.org</p></a>
+          <a className={link} href="https://www.wildlifetrusts.org"><p className={resources}>www.wildlifetrusts.org</p></a>
           <p className={resources}>@WildlifeTrusts</p>
           <p className={resources}>The Wildlife Trusts have a fantastic network of nature reserves and are useful for finding local botanical hotspots. There is plenty of information about our commonest and most intriguing wildflowers on their website.</p>
 
           <p className={resources}><i>The Woodland Trust</i></p>
-          <a className={link} href="www.woodlandtrust.org.uk "><p className={resources}>www.woodlandtrust.org.uk </p></a>
+          <a className={link} href="https://www.woodlandtrust.org.uk "><p className={resources}>www.woodlandtrust.org.uk </p></a>
           <p className={resources}>@WoodlandTrust</p>
           <p className={resources}>The Woodland Trust is a woodland conservation charity that protects, conserves and restores ancient woodland, as well as working to plant new ones.</p>
 
           <p className={resources}><i>Wildflower Hour</i></p>
-          <a className={link} href="www.wildflowerhour.co.uk"><p className={resources}>www.wildflowerhour.co.uk</p></a>
+          <a className={link} href="https://www.wildflowerhour.co.uk"><p className={resources}>www.wildflowerhour.co.uk</p></a>
           <p className={resources}>@wildflowerhour</p>
           <p className={resources}>Wildflower Hour is a social media phenomenon started by Isabel Hardman in 2015. Every Sunday evening between 8 – 9 p.m. people around the country post their favourite wildflower finds from the week using the hashtag #wildflowerhour. Visit the website to find out more.</p>
 
           <p className={resources}><i>British Bryological Society</i></p>
-          <a className={link} href="www.ritishbryologicalsociety.org.uk"><p className={resources}>www.britishbryologicalsociety.org.uk</p></a>
+          <a className={link} href="https://www.ritishbryologicalsociety.org.uk"><p className={resources}>www.britishbryologicalsociety.org.uk</p></a>
           <p className={resources}>@BBSBryology</p>
           <p className={resources}>A wealth of information about mosses, liverworts and hornworts. The BBS has also published an excellent identification guide illustrated with photos.</p>
 
