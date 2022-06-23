1163331279165747d2ca934abd5e10b977136ad9
Haraman Johal <hjohal@oni.bio>
2022-06-23 01:17:58 +0100

    add manifest plugin to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,18 @@ module.exports = {
         }
       },
       {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: `Where the Wildflowers Grow`,
+          short_name: `WTWFG`,
+          start_url: `/`,
+          background_color: `#f7f0eb`,
+          theme_color: `#a2466c`,
+          display: `standalone`,
+          icon: `src/images/icon.png`
+        },
+      },
+      {
         resolve: 'gatsby-plugin-web-font-loader',
         options: {
           google: {
